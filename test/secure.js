c9f9e3b8c74b3ecd0235c1c1ad8ac06df97f207e
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-09 16:34:11 +0000

    Adding test descriptions.

--- test/secure.js
+++ test/secure.js
@@ -3,9 +3,18 @@ var secure = require('..');
 describe('secure', function() {
   describe('#authenticate()', function() {
 
-    // it('should ', function() {
-    //   trunky.truncateWithEllipsis('', 0).should.equal('');
-    // });
+    it('should permit access to granted resources', function() {
+      secure.addResource('Admin');
+      secure.addResource('Admin Bar');
+    });
+
+    it('should disallow access to unspecified resources', function() {
+
+    });
+
+    it('should disallow access to revoked resources', function() {
+
+    });
 
   });
 });
