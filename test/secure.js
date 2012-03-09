b03aecbec0f46aeeacf729d7459829cc30c8c884
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-09 16:51:59 +0000

    Adding tests

--- test/secure.js
+++ test/secure.js
@@ -5,15 +5,30 @@ describe('secure', function() {
 
     it('should permit access to granted resources', function() {
       secure.addResource('Admin');
-      secure.addResource('Admin Bar');
+      secure.grant("jim", "Admin", "read");
+      secure.allowed("jim", "Admin", "read").should.equal(true);
     });
 
-    it('should disallow access to unspecified resources', function() {
-
+    it('should disallow access to unknown resources', function() {
+      secure.allowed("jim", "Unknown resource", "read").should.equal(true);
     });
 
     it('should disallow access to revoked resources', function() {
+      secure.addResource('Admin');
+      secure.grant("jim", "Admin", "read");
+      secure.revoke("jim", "Admin", "read");
+      secure.allowed("jim", "Admin Ding", "read").should.equal(false);
+    });
 
+    it('should disallow access to unknown resources', function() {
+      secure.grant("jim", "Admin", "read");
+      secure.allowed("jim", "Admin Ding", "read").should.equal(false);
+    });
+
+    it('should disallow access to resources granted to other targets', function() {
+      secure.addResource('Admin');
+      secure.grant("jane", "Admin", "read");
+      secure.allowed("jim", "Admin", "read").should.equal(false);
     });
 
   });
