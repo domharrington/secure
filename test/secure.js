a08a545186f75e42f59aa63a161f380e09ad303d
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-09 16:12:23 +0000

    Adding testing template.

---
+++ test/secure.js
@@ -0,0 +1,11 @@
+var secure = require('..');
+
+describe('secure', function() {
+  describe('#authenticate()', function() {
+
+    // it('should ', function() {
+    //   trunky.truncateWithEllipsis('', 0).should.equal('');
+    // });
+
+  });
+});
