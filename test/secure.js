78858c2a31c746b8195fb0d25943a89503c69554
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-15 21:21:10 +0000

    Adding and ordering tests

--- test/secure.js
+++ test/secure.js
@@ -20,16 +20,6 @@ describe('acl', function() {
       acl.allowed("jim", "Unknown resource", "read").should.equal(false);
     });
 
-
-    // TODO: functionality not implemented.
-    // it('should disallow access to revoked resources', function() {
-    //   var acl = require('..').createAccessControlList(mockLogger);
-    //   acl.addResource('Admin');
-    //   acl.grant("jim", "Admin", "read");
-    //   acl.revoke("jim", "Admin", "read");
-    //   acl.allowed("jim", "Admin", "read").should.equal(false);
-    // });
-
     // TODO: failing - should it throw error or return false?
     it('should disallow access to undefined resources when others are defined', function() {
       var acl = require('..').createAccessControlList(mockLogger);
@@ -44,5 +34,68 @@ describe('acl', function() {
       acl.grant("jane", "Admin", "read");
       acl.allowed("jim", "Admin", "read").should.equal(false);
     });
+
+    it('should disallow update access to a resource only read access has been granted to', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin');
+      acl.grant("jim", "Admin", "read");
+      acl.allowed("jim", "Admin", "update").should.equal(false);
+    });
+
+    it('should allow read access to a resource only update access has been granted to', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin');
+      acl.grant("jim", "Admin", "read");
+      acl.allowed("jim", "Admin", "update").should.equal(false);
+    });
+
+    it('should allow update access to a resource when * is granted', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin');
+      acl.grant("jim", "Admin", "*");
+      acl.allowed("jim", "Admin", "update").should.equal(true);
+    });
+
+    // TODO: Failing
+    it('should disallow update access to an unknown resources when * is granted to an unrelated resource', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin');
+      acl.grant("jim", "Admin", "*");
+      acl.allowed("jim", "Unknown", "update").should.equal(true);
+    });
+
+    it('should disallow update access to other resources when * is granted to an unrelated resource', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin');
+      acl.addResource('Other');
+      acl.grant("jim", "Admin", "*");
+      acl.allowed("jim", "Other", "read").should.equal(false);
+    });
+
+    it('should disallow case insensitive operations', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin');
+      acl.grant("jim", "Admin", "*");
+      acl.allowed("jim", "admin", "read").should.equal(false);
+    });
+
+    // TODO: should or shouldn't it?
+    it('should ignore whitespace', function() {
+      var acl = require('..').createAccessControlList(mockLogger);
+      acl.addResource('Admin ');
+      acl.grant("jim", "Admin ", "read");
+      acl.allowed("jim", " Admin", "read").should.equal(true);
+    });
+  });
+
+  describe('#revoke()', function() {
+    // TODO: functionality not implemented.
+    // it('should disallow access to revoked resources', function() {
+    //   var acl = require('..').createAccessControlList(mockLogger);
+    //   acl.addResource('Admin');
+    //   acl.grant("jim", "Admin", "read");
+    //   acl.revoke("jim", "Admin", "read");
+    //   acl.allowed("jim", "Admin", "read").should.equal(false);
+    // });
   });
 });
